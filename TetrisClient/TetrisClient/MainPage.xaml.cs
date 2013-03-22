5bb9d03ecce86d1cb9ecf41a4884b346f31302c5
Simon Stanford <stanfor7@uni.coventry.ac.uk>
2013-03-22 14:27:53 +0000

    Main Files

---
+++ TetrisClient/TetrisClient/MainPage.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Browser;
+
+
+namespace TetrisClient
+{
+    public partial class MainPage : UserControl
+    {
+        List<Rectangle> rectangles;
+        
+
+        int counter = 0; //counter for testing grid
+        private string name;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            name = HtmlPage.Document.QueryString["Name"];
+            lblName.Content = name;
+
+            rectangles = new List<Rectangle>();
+
+            foreach (UIElement child in LayoutRoot.Children)
+            {
+                if (child is Rectangle)
+                    rectangles.Add((Rectangle)child);
+
+            }
+
+            StartTimer(null, null);
+
+
+        }
+
+        //taken from http://msdn.microsoft.com/en-us/library/cc189084%28v=vs.95%29.aspx
+        private void StartTimer(object o, RoutedEventArgs sender)
+        {
+            System.Windows.Threading.DispatcherTimer myDispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            myDispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 5); // 100 Milliseconds 
+            myDispatcherTimer.Tick += new EventHandler(Each_Tick);
+            myDispatcherTimer.Start();
+        }
+
+        // Raised every 100 miliseconds while the DispatcherTimer is active.
+        private void Each_Tick(object o, EventArgs sender)
+        {
+            SolidColorBrush blackBrush = new SolidColorBrush();
+            blackBrush.Color = Colors.Black;
+
+
+            ////SolidColorBrush normalBrush = getBrush("FFF4F4F5");
+            
+
+            if (counter < rectangles.Count)
+            {
+                rectangles[counter].Fill = blackBrush;
+                counter++;
+            }
+            else
+            {
+                string[,] arr = getArray();
+                int i = 0;
+                foreach (string hex in arr)
+                {
+                    rectangles[i].Fill = getBrush(hex);
+                    i++;
+                }
+            }
+            
+
+        }
+
+        //taken from http://stackoverflow.com/questions/6211388/how-to-convert-00e4ff-to-brush-in-code
+        private SolidColorBrush getBrush(string hex)
+        {
+
+            byte r = (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
+            byte g = (byte)(Convert.ToUInt32(hex.Substring(2, 2), 16));
+            byte b = (byte)(Convert.ToUInt32(hex.Substring(4, 2), 16));
+
+            SolidColorBrush myBrush = new SolidColorBrush(Color.FromArgb(255, r, g, b));
+
+            return myBrush;
+        }
+
+        //a test method - should request the string array from the web service
+        private string[,] getArray()
+        {
+            string[,] webServiceData = new string[30, 12] {     { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  },  
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "00FFFF", "00FFFF", "00FFFF", "00FFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  },  
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  },  
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
+                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, };
+
+            return webServiceData;
+        }
+
+        private void UserControl_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Down:
+                    //call game.down() on the web service
+                    lblScore.Content = "down";
+                    Each_Tick(null, null); //this should refresh the grid to make the change quicker
+                    break;
+                case Key.Left:
+                    //call game.left() on the web service
+                    lblScore.Content = "left";
+                    Each_Tick(null, null);
+                    break;
+                case Key.Right:
+                    //call game.right() on the web service
+                    lblScore.Content = "right";
+                    Each_Tick(null, null);
+                    break;
+                case Key.Up:
+                    //call game.rotate() on the web service
+                    lblScore.Content = "rotate";
+                    Each_Tick(null, null);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
