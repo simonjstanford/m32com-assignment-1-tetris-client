4d86f2a1456b272f54aeac9f5bead0d4e9be7597
Simon Stanford <stanfor7@uni.coventry.ac.uk>
2013-03-23 16:25:13 +0000

    Changes to array and added nextShape support

--- TetrisClient/TetrisClient/MainPage.xaml.cs
+++ TetrisClient/TetrisClient/MainPage.xaml.cs
@@ -17,31 +17,43 @@ namespace TetrisClient
 {
     public partial class MainPage : UserControl
     {
-        List<Rectangle> rectangles;
-        
+        List<Rectangle> displayBoard;
+        List<Rectangle> nextShapeBoard;
 
-        int counter = 0; //counter for testing grid
+        int Xcounter = 0; //counter for testing grid
+        int Ycounter = 0; //counter for testing grid
+        int lineCounter = 0;
+
         private string name;
+        private int boardWidth = 12;
+        private int nextShapeBoxWidth = 4;
+
 
         public MainPage()
         {
             InitializeComponent();
 
-            name = HtmlPage.Document.QueryString["Name"];
+            //name = HtmlPage.Document.QueryString["Name"];
             lblName.Content = name;
 
-            rectangles = new List<Rectangle>();
+            displayBoard = new List<Rectangle>();
+            nextShapeBoard = new List<Rectangle>();
 
+            //detect the rectangles and put them in a list - either the board or the next shape box
             foreach (UIElement child in LayoutRoot.Children)
             {
                 if (child is Rectangle)
-                    rectangles.Add((Rectangle)child);
-
+                {
+                    displayBoard.Add((Rectangle)child);
+                }
             }
 
+            for (int i = 360; i < displayBoard.Count; i++)
+                nextShapeBoard.Add(displayBoard[i]);
+
+            displayBoard.RemoveRange(360, 16);
+
             StartTimer(null, null);
-
-
         }
 
         //taken from http://msdn.microsoft.com/en-us/library/cc189084%28v=vs.95%29.aspx
@@ -56,30 +68,50 @@ namespace TetrisClient
         // Raised every 100 miliseconds while the DispatcherTimer is active.
         private void Each_Tick(object o, EventArgs sender)
         {
-            SolidColorBrush blackBrush = new SolidColorBrush();
-            blackBrush.Color = Colors.Black;
 
-
-            ////SolidColorBrush normalBrush = getBrush("FFF4F4F5");
-            
-
-            if (counter < rectangles.Count)
+            if ((Ycounter * boardWidth) + Xcounter < displayBoard.Count)
             {
-                rectangles[counter].Fill = blackBrush;
-                counter++;
+                //this code is only to demonstrate and test the order the squares are filled - can be removed in the release
+                SolidColorBrush blackBrush = new SolidColorBrush();
+                blackBrush.Color = Colors.Black;
+                
+                displayBoard[(Ycounter * boardWidth) + Xcounter].Fill = blackBrush;
+
+                Xcounter++;
+                lineCounter++;
+
+                if (lineCounter == 12)
+                {
+                    lineCounter = 0;
+                    Ycounter++;
+                    Xcounter = 0;
+                }
             }
             else
             {
-                string[,] arr = getArray();
-                int i = 0;
-                foreach (string hex in arr)
-                {
-                    rectangles[i].Fill = getBrush(hex);
-                    i++;
-                }
-            }
-            
+                //this code is the real implementation and must stay in the release - the getArray() method will be called here every second.  
+                //getArray will get the updated array from the web service
+                string[][] webServiceArray = getArray();
 
+                for (int x = 0; x < webServiceArray.Count(); x++)
+                {
+                    for (int y = 0; y < webServiceArray[x].Count(); y++)
+                    {
+                       displayBoard[(y * boardWidth) + x].Fill = getBrush(webServiceArray[x][y]);
+                    }
+                }
+
+                string[][] nextShape = getNextShape();
+                for (int x = 0; x < nextShape.Count(); x++)
+                {
+                    for (int y = 0; y < nextShape[x].Count(); y++)
+                    {
+                        nextShapeBoard[(y * nextShapeBoxWidth) + x].Fill = getBrush(nextShape[x][y]);
+                    }
+                }
+
+
+            }
         }
 
         //taken from http://stackoverflow.com/questions/6211388/how-to-convert-00e4ff-to-brush-in-code
@@ -95,39 +127,40 @@ namespace TetrisClient
             return myBrush;
         }
 
-        //a test method - should request the string array from the web service
-        private string[,] getArray()
+        //a test method - should request the string array from the web service for the board
+        private string[][] getArray()
         {
-            string[,] webServiceData = new string[30, 12] {     { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  },  
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "00FFFF", "00FFFF", "00FFFF", "00FFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  },  
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  },  
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, 
-                                                                { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",  }, };
+            string[][] webServiceData = new string[12][];
+            webServiceData[11] = new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[10] = new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[9] =  new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[8] =  new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[7] =  new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "800080", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[6] =  new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "800080", "800080", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[5] =  new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "800080", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[4] =  new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[3] =  new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[2] =  new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[1] =  new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };          
+            webServiceData[0] =  new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF",};
+
+            
+
+
+            return webServiceData;
+        }
+
+        //a test method - should request the string array from the web service for the next shape
+        private string[][] getNextShape()
+        {
+            string[][] webServiceData = new string[4][];
+            webServiceData[3] = new string[] { "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", };
+            webServiceData[2] = new string[] { "FFFFFF", "FFFFFF", "FF0000", "FFFFFF", };
+            webServiceData[1] = new string[] { "FFFFFF", "FF0000", "FF0000", "FFFFFF", };
+            webServiceData[0] = new string[] { "FFFFFF", "FF0000", "FFFFFF", "FFFFFF", };
+
+
+
 
             return webServiceData;
         }
